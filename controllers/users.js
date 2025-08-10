177b0887dcc50a0b4c6705a27e827ec6b064ab59
k7dbh <203570242+k7dbh@users.noreply.github.com>
2025-08-10 11:48:24 +0300

    Added verifytoken

--- controllers/users.js
+++ controllers/users.js
@@ -4,6 +4,7 @@ const express = require('express');
 
 const router = express.Router();
 const User = require('../models/user');
+const verifyToken = require('../middleware/verify-token')
 
 router.get('/currentUser', async (req, res) => {
   try {
